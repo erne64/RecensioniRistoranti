6938143441b7fdf2833049ab17af22e885cfd676
ErnestoGligora <ernestogligora2003@gmail.com>
2023-11-12 18:48:25 +0100

    Aggiunto postgress pom

---
+++ src/main/java/it/unical/informatica/webapp24/recensioniristoranti/persistenza/DBManager.java
@@ -0,0 +1,22 @@
+package it.unical.informatica.webapp24.recensioniristoranti.persistenza;
+
+import java.sql.Connection;
+
+public class DBManager {
+
+    private static DBManager instance = null;
+
+    private DBManager() {}
+
+    public static DBManager getInstance() {
+        if (instance == null) {
+            instance = new DBManager();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return null;
+    }
+
+}
